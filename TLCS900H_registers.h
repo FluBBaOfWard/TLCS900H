dbe1b905f6de678db062a43a4678094726a9229d
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2022-10-30 18:38:56 +0100

    Fixed register access from C.

--- TLCS900H_registers.h
+++ TLCS900H_registers.h
@@ -11,11 +11,11 @@
 
 
 extern u8 statusRFP;
-extern s8 registersOfsMap[4][256];
+extern const s8 registersOfsMap[4][256];
 extern u8 *currentGprBank;
 
 #define rCodeB(r)	(*(currentGprBank+registersOfsMap[statusRFP][(r)]))
-#define rCodeW(r)	(*((u16*)currentGprBank+registersOfsMap[statusRFP][(r) & ~1]))
-#define rCodeL(r)	(*((u32*)currentGprBank+registersOfsMap[statusRFP][(r) & ~3]))
+#define rCodeW(r)	(*((u16*)(currentGprBank+registersOfsMap[statusRFP][(r) & ~1])))
+#define rCodeL(r)	(*((u32*)(currentGprBank+registersOfsMap[statusRFP][(r) & ~3])))
 
 #endif	// __TLCS900H_REGISTERS__
